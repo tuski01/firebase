13a703eb212bfecf544d99f59a7daef442995320
king <tuski0@g.shingu.ac.kr>
2023-06-13 11:53:08 +0900

    회원가입 수정

--- app/src/main/java/net/flow9/dcjt/firebase/PostActivity.java
+++ app/src/main/java/net/flow9/dcjt/firebase/PostActivity.java
@@ -3,6 +3,7 @@ package net.flow9.dcjt.firebase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -12,14 +13,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PostActivity extends AppCompatActivity {
     private Spinner spinner;
+    private ArrayAdapter categoryAA, brandAA;
+    private Spinner categorySp, brandSp;
+    private String catogory="", brand="";
+
     TextView tv_result;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        spinner = findViewById(R.id.spinner);
+        categorySp = findViewById(R.id.spinner);
+        brandSp = findViewById(R.id.spinner2);
+
+        categoryAA = ArrayAdapter.createFromResource(this, R.array.category, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        categorySp.setAdapter(categoryAA);
+
+        brandAA = ArrayAdapter.createFromResource(this, R.array.brand, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        brandSp.setAdapter(brandAA);
+
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
