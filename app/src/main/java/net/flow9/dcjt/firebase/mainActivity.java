2a0f0437832f3c7542a7a8c30ea98dd6e7ded36f
king <tuski0@naver.com>
2023-06-20 11:20:28 +0900

    메인화면 이미지 땡겨오기

--- app/src/main/java/net/flow9/dcjt/firebase/mainActivity.java
+++ app/src/main/java/net/flow9/dcjt/firebase/mainActivity.java
@@ -1,5 +1,4 @@
 package net.flow9.dcjt.firebase;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,14 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,16 +25,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
-
 import net.flow9.dcjt.firebase.adapters.find_PostAdapter;
 import net.flow9.dcjt.firebase.adapters.lost_PostAdapter;
 import net.flow9.dcjt.firebase.model.Post;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class mainActivity extends Fragment implements View.OnClickListener {
-
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
     private FirebaseStorage mStore = FirebaseStorage.getInstance();
@@ -47,7 +44,9 @@ public class mainActivity extends Fragment implements View.OnClickListener {
     private Animation fab_open, fab_close;
     private boolean isFabOpen = false;
     private Context mContext;
+    private FirebaseUser user = mAuth.getCurrentUser();
 
+    private GridLayoutManager layoutmaneger1, layoutmaneger2;
 
 
 
@@ -56,34 +55,35 @@ public class mainActivity extends Fragment implements View.OnClickListener {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_main, container, false);
 
+
         mContext = view.getContext();
+        layoutmaneger1 = new GridLayoutManager(getActivity(), 2);
+        layoutmaneger1.setReverseLayout(true);
+        layoutmaneger2 = new GridLayoutManager(getActivity(), 2);
+        layoutmaneger2.setReverseLayout(true);
+
 
         fab_open = AnimationUtils.loadAnimation(mContext, R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(mContext, R.anim.fab_close);
-
         fab_main =  view.findViewById(R.id.fab_main);
         fab_sub1 =  view.findViewById(R.id.fab_sub1);
         fab_sub2 =  view.findViewById(R.id.fab_sub2);
-
         fab_main.setOnClickListener(this);
         fab_sub1.setOnClickListener(this);
         fab_sub2.setOnClickListener(this);
-
-
         fab_main.setOnClickListener(this);
         fab_sub1.setOnClickListener(this);
         fab_sub2.setOnClickListener(this);
-
         find_object_list();
         lost_object_list();
-
         return view;
     }
 
 
     public void find_object_list(){
-        Query myTopPostsQuery = mDatabase.child("firebase").child("Find_object");
+        Query myTopPostsQuery = mDatabase.child("firebase").child("Find_object").limitToLast(4);
         fPostRecyclerView = view.findViewById(R.id.main_recycleview);
+        fPostRecyclerView.setLayoutManager(layoutmaneger1);
         mDatas1 = new ArrayList<>();
         myTopPostsQuery.addValueEventListener(new ValueEventListener() {
             @Override
@@ -94,6 +94,8 @@ public class mainActivity extends Fragment implements View.OnClickListener {
                     mDatas1.add(postdata);
                 }
                 fAdapter = new find_PostAdapter(mDatas1);
+                GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+                fPostRecyclerView.setLayoutManager(gridLayoutManager);
                 fPostRecyclerView.setAdapter(fAdapter);
             }
 
@@ -103,8 +105,9 @@ public class mainActivity extends Fragment implements View.OnClickListener {
         });
     }
     public void lost_object_list(){
-        Query myTopPostsQuery = mDatabase.child("firebase").child("Lost_object");
+        Query myTopPostsQuery = mDatabase.child("firebase").child("Lost_object").limitToLast(4);
         lPostRecyclerView = view.findViewById(R.id.main_recycleview2);
+        lPostRecyclerView.setLayoutManager(layoutmaneger2);
         mDatas2 = new ArrayList<>();
         myTopPostsQuery.addValueEventListener(new ValueEventListener() {
             @Override
@@ -114,10 +117,11 @@ public class mainActivity extends Fragment implements View.OnClickListener {
                     Post postdata = dataSnapshot.getValue(Post.class);
                     mDatas2.add(postdata);
                 }
+                GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+                fPostRecyclerView.setLayoutManager(gridLayoutManager);
                 lAdapter = new lost_PostAdapter(mDatas2);
                 lPostRecyclerView.setAdapter(lAdapter);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
@@ -134,12 +138,7 @@ public class mainActivity extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
     }
-
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -148,13 +147,24 @@ public class mainActivity extends Fragment implements View.OnClickListener {
                 break;
             case R.id.fab_sub1:
                 toggleFab();
-                Intent intent = new Intent(getActivity(), Lost_Post_Activity.class);
-                startActivity(intent);
+                if(user != null) {
+                    Intent intent = new Intent(getActivity(), Lost_Post_Activity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent_start = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent_start);
+                }
                 break;
             case R.id.fab_sub2:
                 toggleFab();
-                Intent intent2 = new Intent(getActivity(), Find_Post_Activity.class);
-                startActivity(intent2);
+                if(user != null) {
+
+                    Intent intent2 = new Intent(getActivity(), Find_Post_Activity.class);
+                    startActivity(intent2);
+                }else {
+                    Intent intent_start2 = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent_start2);
+                }
                 break;
         }
     }
@@ -171,7 +181,6 @@ public class mainActivity extends Fragment implements View.OnClickListener {
             fab_sub1.setClickable(true);
             fab_sub2.setClickable(true);
             isFabOpen = true;
-
         }
     }
 }
